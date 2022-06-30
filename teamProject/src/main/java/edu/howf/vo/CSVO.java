86899b6182ce9e86daf2d9166e771cca83203f64
315 <315@DESKTOP-LHQUB49>
2022-06-30 11:50:30 +0900

    CSVO update

--- teamProject/src/main/java/edu/howf/vo/CSVO.java
+++ teamProject/src/main/java/edu/howf/vo/CSVO.java
@@ -1,5 +1,56 @@
 package edu.howf.vo;
 
 public class CSVO extends BoardVO{
+	
+//	private String title;
+//	private String content;
+//	private String writer;
+//	private String wdate;
+//	private int cnt;
+//	private String delyn;
+	private int midx;	//유저 번호
+	private int csidx;	//1:1 문의글 번호
+	private int ridx;
+	private String divsn;
+	private String filename;
+	private String replyn;
+	
+	
+	public int getMidx() {
+		return midx;
+	}
+	public void setMidx(int midx) {
+		this.midx = midx;
+	}
+	public int getCsidx() {
+		return csidx;
+	}
+	public void setCsidx(int csidx) {
+		this.csidx = csidx;
+	}
+	public int getRidx() {
+		return ridx;
+	}
+	public void setRidx(int ridx) {
+		this.ridx = ridx;
+	}
+	public String getDivsn() {
+		return divsn;
+	}
+	public void setDivsn(String divsn) {
+		this.divsn = divsn;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getReplyn() {
+		return replyn;
+	}
+	public void setReplyn(String replyn) {
+		this.replyn = replyn;
+	}
 
 }
