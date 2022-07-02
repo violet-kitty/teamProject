00083c250837fa2bf7f2fee9695590fd7720025a
KHJ <315@DESKTOP-LHQUB49>
2022-07-02 12:57:43 +0900

    CS 관련 업데이트 07-02 12:57

--- teamProject/src/main/java/edu/howf/dao/CSDao.java
+++ teamProject/src/main/java/edu/howf/dao/CSDao.java
@@ -1,13 +1,38 @@
 package edu.howf.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import edu.howf.vo.CSVO;
+import edu.howf.vo.SearchVO;
+
 @Repository
 public class CSDao {
+	
 	@Autowired
 	SqlSession sqlSession;
 	
-	String namespace = "edu.howf.mapper.csMapper";
+//	private static final String namespace = "edu.howf.mapper.csMapper";
+//	
+//	public List<CSVO> CSList(SearchVO vo){
+//		
+//		return sqlSession.selectList(namespace + ".1v1_selectAll", vo);
+//	}
+//	
+//	public int countPage(SearchVO vo) {
+//		
+//		return sqlSession.selectOne(namespace + ".countPage", vo);
+//	}
+//	
+//	public int CS_write(CSVO vo) {
+//		
+//		int csbidx = sqlSession.insert(namespace + "CS_write", vo);
+//		
+//		return sqlSession.update(namespace + "origincsbidx_update", csbidx);
+//	}
+	
+	
 }
--- teamProject/src/main/java/edu/howf/service/CSServiceImple.java
+++ teamProject/src/main/java/edu/howf/service/CSServiceImple.java
@@ -1,12 +1,37 @@
 package edu.howf.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.howf.dao.CSDao;
+import edu.howf.vo.CSVO;
+import edu.howf.vo.SearchVO;
 
 @Service
 public class CSServiceImple implements CSService{
+	
 	@Autowired
 	CSDao csDao;
+
+//	@Override
+//	public List<CSVO> CSList(SearchVO vo) {
+//		
+//		return csDao.CSList(vo);
+//	}
+//
+//	@Override
+//	public int countPage(SearchVO vo) {
+//		
+//		return csDao.countPage(vo);
+//	}
+//
+//	@Override
+//	public int CS_write(CSVO vo) {
+//		
+//		return csDao.CS_write(vo);
+//	}
+	
+	
 }
