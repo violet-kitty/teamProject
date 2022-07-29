1307c00fa6e97bd1cf1cf7f7ca3adf5982eec893
yujin <yujin@yujin>
2022-07-29 16:07:55 +0900

    모달

--- teamProject/src/main/webapp/resources/js/modal.js
+++ teamProject/src/main/webapp/resources/js/modal.js
@@ -1,7 +1,7 @@
 //모달 띄우는 function
 
 
-function modalFn(msg, btnMsg1){
+function modalFn(msg, btnMsg1, title){
 	var length = arguments.length;
 	
 	//아무 버튼 없이 글만 뜨는 모달
@@ -25,6 +25,26 @@ function modalFn(msg, btnMsg1){
 		var html = '<div class="modal py-10 px-3" tabindex="-1" role="dialog" id="modalDiv">'
 					+ '<div class="modal-dialog hbshadow" role="document">'
 					+ '<div class="modal-content">'
+					+ '<div class="form-floating ">'
+					+ '<p class="text-muted">'+msg+'</p>'
+					+ '</div>'
+					+ '<div class="btnarea">'
+					+ '<button type="button" class="bluebtn w-100" onclick="modalClose()">'+btnMsg1+'</button>'
+					+ '</div>'
+					+ '</div>'
+					+ '</div>'
+					+ '</div>';
+		
+		$("body").prepend(html);
+		
+		$("#modalDiv").show();
+	}
+	//타이틀과 버튼이 있는 모달
+	else if(length == 3){
+		var html = '<div class="modal py-10 px-3" tabindex="-1" role="dialog" id="modalDiv">'
+					+ '<div class="modal-dialog hbshadow" role="document">'
+					+ '<div class="modal-content">'
+					+ '<h2 class="hfc-bold">'+title+'</h2>'
 					+ '<div class="form-floating ">'
 					+ '<p class="text-muted">'+msg+'</p>'
 					+ '</div>'
